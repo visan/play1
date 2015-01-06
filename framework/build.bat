6fcb80c480c24f413e7b2e6669b9bdc97d1a771b
visan <alievdenis@gmail.com>
2015-01-07 01:24:37 +0600

    * module quick compile feature

--- framework/build.bat
+++ framework/build.bat
@@ -1,8 +1,8 @@
-call setenv.cmd
+rem call setenv.cmd
 
-rem set ANT_HOME=D:\java\apache-ant-1.9.4
-rem set JAVA_HOME=D:\java\jdk6-45-b64
-
+set ANT_HOME=F:\java\apache-ant-1.9.4
+rem set JAVA_HOME=F:\java\jdk6-45-b64
+set JAVA_HOME=C:\java\jdk-9.25-b64
 
 
 %ANT_HOME%/bin/ant -l build.log -v -f build.xml package
